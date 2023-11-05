7e44ca36203681dce569b26b1d7d8ac66ef40d1f
Tejas Jaybhaye <117623822+tejayy@users.noreply.github.com>
2023-11-05 21:56:11 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 
 function App() {
+  const url = "https://api.adviceslip.com/advice";
+
+  const [data, setData] = useState([]);
+
+  const fetchInfo = () => {
+    return fetch(url)
+    .then(res => res.json())
+    .then(data => setData(data.slip.advice))
+  }
+
+
+  useEffect (() => {
+    fetchInfo();
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+        <h1 className='heading' >{data}</h1>
+        <button className="btn" onClick={fetchInfo}>
+          <span>Give Me Advice</span>
+        </button>
+      </div>
     </div>
   );
 }
